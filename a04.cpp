17dab6690558b869a086ff978d36e6c789683f6f
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2023-03-04 22:05:10 +0530

    Update a04.cpp

--- a04.cpp
+++ a04.cpp
@@ -1,66 +1,88 @@
 #include <stdio.h>
-#include <cuda_runtime.h>
 
-#define N 1024 // Vector and matrix size
-
-__global__ void vectorAdd(int *a, int *b, int *c)
-{
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < N)
+// Vector addition kernel
+__global__ void vectorAdd(int* a, int* b, int* c, int size) {
+    int i = threadIdx.x;
+    if (i < size) {
         c[i] = a[i] + b[i];
+    }
 }
 
-__global__ void matrixMul(int *a, int *b, int *c)
-{
+// Matrix multiplication kernel
+__global__ void matrixMul(int* a, int* b, int* c, int size) {
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
-    if (row < N && col < N)
-    {
+    if (row < size && col < size) {
         int sum = 0;
-        for (int k = 0; k < N; k++)
-            sum += a[row * N + k] * b[k * N + col];
-        c[row * N + col] = sum;
+        for (int k = 0; k < size; k++) {
+            sum += a[row * size + k] * b[k * size + col];
+        }
+        c[row * size + col] = sum;
     }
 }
 
-int main()
-{
-    int *a, *b, *c, *d, *e, *f;
-    cudaMallocManaged(&a, N * N * sizeof(int));
-    cudaMallocManaged(&b, N * N * sizeof(int));
-    cudaMallocManaged(&c, N * N * sizeof(int));
-    cudaMallocManaged(&d, N * sizeof(int));
-    cudaMallocManaged(&e, N * sizeof(int));
-    cudaMallocManaged(&f, N * sizeof(int));
-    for (int i = 0; i < N; i++)
-    {
-        d[i] = i;
-        e[i] = N - i;
-    }
-    for (int i = 0; i < N * N; i++)
-    {
+int main() {
+    // Vector addition and matrix multiplication setup
+    const int VECTOR_SIZE = 1000000;
+    const int MATRIX_SIZE = 1000;
+    int* a = new int[VECTOR_SIZE];
+    int* b = new int[VECTOR_SIZE];
+    int* c = new int[VECTOR_SIZE];
+    int* x = new int[MATRIX_SIZE * MATRIX_SIZE];
+    int* y = new int[MATRIX_SIZE * MATRIX_SIZE];
+    int* z = new int[MATRIX_SIZE * MATRIX_SIZE];
+    int* d_a, *d_b, *d_c, *d_x, *d_y, *d_z;
+    cudaMalloc(&d_a, VECTOR_SIZE * sizeof(int));
+    cudaMalloc(&d_b, VECTOR_SIZE * sizeof(int));
+    cudaMalloc(&d_c, VECTOR_SIZE * sizeof(int));
+    cudaMalloc(&d_x, MATRIX_SIZE * MATRIX_SIZE * sizeof(int));
+    cudaMalloc(&d_y, MATRIX_SIZE * MATRIX_SIZE * sizeof(int));
+    cudaMalloc(&d_z, MATRIX_SIZE * MATRIX_SIZE * sizeof(int));
+    for (int i = 0; i < VECTOR_SIZE; i++) {
         a[i] = i;
-        b[i] = N * N - i;
+        b[i] = i * i;
     }
-    vectorAdd<<<(N + 255) / 256, 256>>>(d, e, f);
-    matrixMul<<<dim3((N + 15) / 16, (N + 15) / 16), dim3(16, 16)>>>(a, b, c);
-    cudaDeviceSynchronize();
+    for (int i = 0; i < MATRIX_SIZE * MATRIX_SIZE; i++) {
+        x[i] = i;
+        y[i] = i * i;
+    }
+    cudaMemcpy(d_a, a, VECTOR_SIZE * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, VECTOR_SIZE * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_x, x, MATRIX_SIZE * MATRIX_SIZE * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_y, y, MATRIX_SIZE * MATRIX_SIZE * sizeof(int), cudaMemcpyHostToDevice);
+
+    // Vector addition execution
+    vectorAdd<<<1, VECTOR_SIZE>>>(d_a, d_b, d_c, VECTOR_SIZE);
+    cudaMemcpy(c, d_c, VECTOR_SIZE * sizeof(int), cudaMemcpyDeviceToHost);
     printf("Vector addition result:\n");
-    for (int i = 0; i < N; i++)
-        printf("%d ", f[i]);
-    printf("\n");
-    printf("Matrix multiplication result:\n");
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-            printf("%d ", c[i * N + j]);
-        printf("\n");
+    for (int i = 0; i < VECTOR_SIZE; i++) {
+        printf("%d ", c[i]);
     }
-    cudaFree(a);
-    cudaFree(b);
-    cudaFree(c);
-    cudaFree(d);
-    cudaFree(e);
-    cudaFree(f);
+    printf("\n");
+
+    // Matrix multiplication execution
+    printf("\nMatrix multiplication result:\n");
+    for (int i = 0; i < MATRIX_SIZE * MATRIX_SIZE; i++) {
+        if (i % MATRIX_SIZE == 0 && i > 0) {
+            printf("\n");
+        }
+        printf("%d ", z[i]);
+    }
+    printf("\n");
+
+    // Free memory
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+    cudaFree(d_x);
+    cudaFree(d_y);
+    cudaFree(d_z);
+    delete[] a;
+    delete[] b;
+    delete[] c;
+    delete[] x;
+    delete[] y;
+    delete[] z;
+
     return 0;
 }
